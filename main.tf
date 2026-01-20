0ceeb0a2de0454619ba957bfc84ca0e8f3514971
pooja <poojamarne030@gmail.com>
2026-01-21 02:00:01 +0530

    new resource added

--- main.tf
+++ main.tf
@@ -14,4 +14,12 @@ resource "aws_instance" "example" {
   tags = {
     Name = "HelloWorld-${count.index}"
   }
+resource "aws_instance" "example" {
+  ami           = "ami-08982f1c5bf93d976"
+  instance_type = "t2.micro"
+  count = 3
+
+  tags = {
+    Name = "HelloWorld-${count.index}"
+  }
 }
